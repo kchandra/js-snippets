c79e84867b7cb2ee1902fc430c6fc12d0cfe9cb1
kushal chandra <kchandra@expedia.com>
2020-05-16 16:52:41 +0000

    added object example

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ const add = (a,b) => a+b;
 var sum = nums.reduce(add);
 console.log(sum)
 
-const reductions = {}
+const reductions = [
+  {
+    offerRequestId: "X1-A",
+    address: "123 Main St"
+  },
+  {
+    offerRequestId: "X1-B",
+    address: "567 Main St"
+  },
+  {
+    offerRequestId: "X1-C",
+    address: "123 1st Ave"
+  }
+]
+
+const reductionsByOrid = reductions.reduce((acc, cv)=>{
+  acc[cv.offerRequestId] = cv;
+  return acc;
+}, {})
+
+console.log(reductionsByOrid);
